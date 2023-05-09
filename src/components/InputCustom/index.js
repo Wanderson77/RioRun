b66b729822c03a6769e7e8a9fcc378328d3ce920
Wanderson77 <wanderson.aldo@gmail.com>
2023-05-09 11:37:14 -0300

    My Custom Components

---
+++ src/components/InputCustom/index.js
@@ -0,0 +1,18 @@
+import {
+  View,
+  Text,
+  Image
+} from 'react-native'
+import React from 'react'
+
+export default function InputCustom() {
+
+  renderTodoList = () => {};
+
+  return (
+    <View>
+      <Text>index</Text>
+      <Image></Image>
+    </View>
+  )
+}
--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,61 +1,15 @@
-/** Initial Screen */
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-const Home = ({
-  route
-}) => {
-  const {
-    item
-  } = route.params;
-  return ( <
-    View style = {
-      styles.container
-    } >
-    <
-    View style = {
-      styles.contentContainer
-    } >
-    <
-    Text style = {
-      styles.title
-    } > Home Delivery address: < /Text> <
-    Text style = {
-      styles.text
-    } > {
-      item
-    } < /Text> <
-    /View> <
-    /View>
-  );
-};
+import InputCustom from '../../components/InputCustom'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#070707',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  contentContainer: {
-    paddingHorizontal: 20,
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: '700',
-    color: '#FD0139',
-    paddingBottom: 10
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: '400',
-    color: '#fff'
-  }
-});
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <InputCustom />
+    </View>
+  )
+}
 
-export default Home;
+export default index
